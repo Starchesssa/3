a6e1364581b9441e33164525b52c96dd7cfc1ae3
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-07-13 23:01:44 +0300

    Update Overlay_test.py

--- Overlay_test.py
+++ Overlay_test.py
@@ -5,7 +5,7 @@ import subprocess
 input_video = "group_21.mp4"
 output_video = "output.mp4"
 text_string = "Smart Pet-feeder"
-font_path = "FontsFree-Net-Proxima-Nova-Bold-It.otf.ttf"  # custom font
+font_path = "Milkyway DEMO.ttf"  # custom font
 
 # === FADE IN / FADE OUT ALPHA EXPRESSION ===
 fade_alpha = (
