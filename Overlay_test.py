9fa35b06b2d8886be010edc3f82fbefa8e2b0c84
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-07-13 08:08:42 +0300

    Update Overlay_test.py

--- Overlay_test.py
+++ Overlay_test.py
@@ -18,7 +18,7 @@ ScriptType: v4.00+
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,{font_name},{fontsize},&H00FFFFFF,&H000000FF,&H00000000,&H00000000,1,0,0,0,100,100,0,0,1,2,2,2,10,10,10,1
+Style: Default,{font_name},{fontsize},&H0000C5FF,&H000000FF,&H00000000,&H64000000,1,0,0,0,100,100,0,0,1,2,2,2,10,10,10,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
