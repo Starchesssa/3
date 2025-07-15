69e13735d7923191735b28f1b11b0e68bfb81ba5
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-07-15 21:51:50 +0300

    Update Unusual_rating.py

--- Unusual_rating.py
+++ Unusual_rating.py
@@ -23,7 +23,7 @@ API_KEYS = [key for key in API_KEYS if key]
 if not API_KEYS:
     raise ValueError("❌ No valid GEMINI_API keys found.")
 
-MODEL = "gemini-2.5-flash"
+MODEL = "gemini-2.5-flash-lite-preview-06-17"
 os.makedirs(RATING_DIR, exist_ok=True)
 
 # === Prompt ===
