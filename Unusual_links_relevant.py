7893a4cbeb8e3f62d34ab4f58e9cc65e73f430e7
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-07-16 08:15:09 +0300

    Update Unusual_links_relevant.py

--- Unusual_links_relevant.py
+++ Unusual_links_relevant.py
@@ -28,7 +28,7 @@ API_KEYS = [key for key in API_KEYS if key]
 if not API_KEYS:
     raise ValueError("❌ No valid GEMINI_API keys found.")
 
-MODEL = "gemini-2.5-flash-lite-preview-06-17"
+MODEL = "gemini-2.5-flash"
 
 os.makedirs(RELEVANT_DIR, exist_ok=True)
 
