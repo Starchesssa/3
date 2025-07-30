ec1f6939bd24b4ed808adcd858cfb3971303bc41
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-07-30 08:59:24 +0300

    Update comp2.py

--- BOOK_CODE/COMPANY_BIO/comp2.py
+++ BOOK_CODE/COMPANY_BIO/comp2.py
@@ -46,11 +46,12 @@ def generate_chapters(book):
         "list all numbered chapter names  of the book.\n"
         "Number each chapter starting from 1.make sure you list all chapters \n"
         "if the book has no chapters ,make themed chapters for it for my audience to understand the book"
-        "chapters must be 7 and above so that my audience may clearly understand , if the official chapters contian less then no problem"
+        "chapters must be 7 and above so that my audience may clearly understand , if the official chapters contain less then no problem"
         "Example format:\n"
         "1. The problem\n"
         "2. Solution Comes\n"
         "3. Succes \n"
+        "always arrange each chapter in its own line , dont make two chapters in same line,arrange the chapters in a list,each chapter on its own line"
         "Only provide the numbered chapters text without extra commentary,just chapter only nothing more."
     )
 
