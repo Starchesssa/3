66eff6870c7f00b0ed2724fa408eb375ce40b0c9
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-11-04 06:59:02 +0300

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,6 +1,8 @@
 
+// remotion.config.ts
 import { Config } from "@remotion/cli/config";
 
 Config.setVideoImageFormat("jpeg");
 Config.setCodec("h264");
-Config.setFfmpegOverride(() => "ffmpeg");
+
+// Config.setFfmpegOverride(() => "ffmpeg"); // ❌ remove this line in v4
