48f7d85b5595e4dbd1349a72555f19e2c9128519
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-11-11 16:27:02 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,5 @@
-import { registerRoot } from 'remotion';
-import { Composition } from 'remotion';
-import React from 'react';
+// src/index.tsx
+import {registerRoot} from 'remotion';
+import {RemotionVideo} from './DepthShowcase';
 
-// Import your actual scene component
-import { ParallaxThree } from './ParallaxThree';
-
-const RemotionRoot: React.FC = () => {
-  return (
-    <>
-      <Composition
-        id="ParallaxThree"           // Use the correct ID
-        component={ParallaxThree}    // Must match the imported component
-        durationInFrames={240}
-        fps={24}
-        width={1920}
-        height={1080}
-      />
-    </>
-  );
-};
-
-// Register the root
-registerRoot(RemotionRoot);
+registerRoot(RemotionVideo);
