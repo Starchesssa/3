0dab1ca3de18734cdb59657038f5cb3de688a93a
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-11-16 14:29:22 +0300

    Update comp3.py

--- BOOK_CODE/COMPANY_BIO/comp3.py
+++ BOOK_CODE/COMPANY_BIO/comp3.py
@@ -42,15 +42,16 @@ def load_company_bio():
 # === Build AI Prompt ===
 def build_prompt(info):
     return f"""
-You are writing a long storytelling script based on the book titled '{info['title']}' by {info['author']}.
+You are writing a long storytelling Magnettes Media youtube script based on the book titled '{info['title']}' by {info['author']}.
 The story focuses on the company {info['company']}.
 
 Rules:
 1. Use Magnettes Media YouTube narration style..
-2. EACH lesson must begin with a CAPITALIZED HEADING.
+2. EACH lesson must begin with a CAPITALIZED HEADING ie LESSOB 1: XYZ ,number each lesson .
 3. Every sentence must end with a full stop(.) .
 4. dont begin with any into jist output the script only, no scenes ,no anything just plain script.
 5. Use 'you', 'your', 'yours'. Never use 'I' or 'we'.
+6. Make the script to be long for atleast 200 sentences or above.
 6. ONLY use the company name given: {info['company']}.
 
 Begin the long script now.
