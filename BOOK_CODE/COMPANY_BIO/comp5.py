993de25ba294faa8a30e070bb2e9b25a3277dc23
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-11-22 21:43:11 +0300

    Update comp5.py

--- BOOK_CODE/COMPANY_BIO/comp5.py
+++ BOOK_CODE/COMPANY_BIO/comp5.py
@@ -45,7 +45,7 @@ The timeline is in seconds, e.g. 0.00 --> 0.36 means 0.00 to 0.35 seconds.
 
 Your task:,here is my transcipt, give me image  description for the timeline using 
 all character you make will have angular head with no eyes,no nose, no mouth , no ears, just an abstract angular black shape 
-make all image decriptions match alux.com style ,here is the formular (3D character/s + pose of the charcter/s+environment) all in parallax),this is the formular ,
+make all image decriptions match alux.com style ,here is the formular (3D silhoutte + pose of the charcter/s+environment) all in parallax),this is the formular ,
 
 its simple alux.com nomally use vector angular sharp geometric  in clothes  and environment in parallax , but more important pose estimation  in the image based on the emotion  this is very important .
 
