1cf5daa150dfaa18eb256ee9da2f3737ae9cf5e9
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-12-30 07:54:04 +0300

    Update M.py

--- MANIM/M.py
+++ MANIM/M.py
@@ -1,12 +1,13 @@
+
 from manim import *
 import os
 
 class PNGZDepthSequence(ThreeDScene):
     def construct(self):
         # Camera setup
-        self.set_camera_orientation(phi=70*DEGREES, theta=-90*DEGREES)
+        self.set_camera_orientation(phi=75*DEGREES, theta=0*DEGREES)
+        self.begin_ambient_camera_rotation(rate=0.03)
 
-        # Load PNGs
         img_dir = "MANIM/Imag_samples"
         image_files = sorted(f for f in os.listdir(img_dir) if f.endswith(".png"))
         if not image_files:
@@ -14,24 +15,36 @@ class PNGZDepthSequence(ThreeDScene):
 
         z_step = 5
         img_scale = 2.8
+        fade_time = 1
+        hold_time = 1.5
         fly_time = 2.5
+
         current_z = 0
+        last_image = None
 
         for img in image_files:
-            im = ImageMobject(os.path.join(img_dir, img)).scale(img_scale)
-            im.move_to([0, 0, current_z])
-            self.add(im)
-
-            # Fade in and hold
-            self.play(FadeIn(im), run_time=1)
-            self.wait(2.5)
-
-            # Camera glides forward
-            self.play(
-                self.camera.frame.animate.move_to([0, 0, current_z]),
-                run_time=fly_time
+            im = (
+                ImageMobject(os.path.join(img_dir, img))
+                .scale(img_scale)
+                .move_to([0, 0, current_z])
             )
 
+            self.add(im)
+            self.play(FadeIn(im), run_time=fade_time)
+            self.wait(hold_time)
+
+            # Camera moves forward THROUGH space
+            self.play(
+                self.camera.frame.animate.shift(OUT * z_step),
+                run_time=fly_time,
+                rate_func=smooth
+            )
+
+            # Remove old image
+            if last_image:
+                self.remove(last_image)
+
+            last_image = im
             current_z += z_step
 
         self.wait(1)
